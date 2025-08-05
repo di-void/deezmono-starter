e42b9fe02ca91e59191a26f65a260db8c586f8fe
DEE <donzydonathan@gmail.com>
2025-08-05 14:39:47 +0100

    refactor: server app configs

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,14 +1,12 @@
-import { add, bar } from "@acme/add";
+import { addTimes } from "@acme/add";
 import { minus } from "@acme/add/lib";
 
 function main() {
-  const res = add(1, 2);
-  const bars = bar();
-  const mRes = minus(2, 1);
+  const addResult = addTimes(1, 2);
+  const minusResult = minus(2, 1);
 
-  console.log("Result:", res);
-  console.log("Result:", mRes);
-  console.log("Result:", bars);
+  console.log("Add Reusult:", addResult);
+  console.log("Minus Reusult:", minusResult);
 }
 
 main();
